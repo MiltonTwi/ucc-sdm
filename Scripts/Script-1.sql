429d1073f0e93edac29f98156c45d2e4dec103fb
MiltonTwi <milton.quinteroes@campusucc.edu.co>
2024-10-12 18:50:22 -0500

    Trabajo en clase 10/12/24

---
+++ Scripts/Script-1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.dummy (
+	id serial4 NOT NULL,
+	"name" varchar(30) NULL
+);
+
+INSERT INTO dummy (id, name) VALUES (1, 'John Doe');
+INSERT INTO dummy (id, name) VALUES (2, 'Jane Smith');
